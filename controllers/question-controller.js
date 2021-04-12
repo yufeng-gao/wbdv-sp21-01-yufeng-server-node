ecc4c04e2a90981e3352ec03b71e0f493d425f90
yufeng-gao <77986637+yufeng-gao@users.noreply.github.com>
2021-04-11 23:17:00 -0400

    Fixed a syntax error in controllers

--- controllers/question-controller.js
+++ controllers/question-controller.js
@@ -3,10 +3,9 @@ const questionService
 module.exports = (app) => {
     const findQuestionsForQuiz = (req, res) => {
         const quizId = req.params['qid']
-        questionService.findQuestionsForQuiz(quizId)
-        .then ((questions) => {
-            res.send(questions)
-        })
+        const questions
+            = questionService.findQuestionsForQuiz(quizId)
+        res.json(questions)
     }
     app.get("/api/quizzes/:qid/questions",
         findQuestionsForQuiz)
--- controllers/quizzes-controller.js
+++ controllers/quizzes-controller.js
@@ -1,19 +1,12 @@
 const quizService
     = require('../services/quiz-service')
 module.exports = (app) => {
-    const findAllQuizzes = (req, res) => {
-        quizService.findAllQuizzes()
-            .then((quizzes) => {
-                res.send(quizzes)
-            })
-    }
-
+    const findAllQuizzes = (req, res) =>
+        res.send(quizService.findAllQuizzes())
     const findQuizById = (req, res) => {
         const quizId = req.params['qid']
-        quizService.findQuizById(quizId)
-            .then((quiz) => {
-                res.send(quiz)
-            })
+        const quiz = quizService.findQuizById(quizId)
+        res.json(quiz)
     }
     app.get('/api/quizzes', findAllQuizzes)
     app.get('/api/quizzes/:qid', findQuizById)
